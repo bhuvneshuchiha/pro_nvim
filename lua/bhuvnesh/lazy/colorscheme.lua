06743a2fef0aa2b0cd2dcf305eabe3cdfd6e6323
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-06-10 15:55:26 +0530

    "Few changes lsp, autocommands etc"

--- lua/bhuvnesh/lazy/colorscheme.lua
+++ lua/bhuvnesh/lazy/colorscheme.lua
@@ -45,38 +45,38 @@
 -- 	end,
 -- }
 --
-return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	config = function()
-		require("gruvbox").setup({
-			terminal_colors = true, -- add neovim terminal colors
-			undercurl = true,
-			underline = true,
-			bold = true,
-			italic = {
-				strings = false,
-				emphasis = false,
-				comments = false,
-				operators = false,
-				folds = false,
-			},
-			strikethrough = true,
-			invert_selection = false,
-			invert_signs = false,
-			invert_tabline = false,
-			invert_intend_guides = false,
-			inverse = true, -- invert background for search, diffs, statuslines and errors
-			contrast = "", -- can be "hard", "soft" or empty string
-			palette_overrides = {},
-			overrides = {},
-			dim_inactive = false,
-			transparent_mode = true,
-		})
-		vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd("colorscheme gruvbox")
-	end,
-}
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("gruvbox").setup({
+-- 			terminal_colors = true, -- add neovim terminal colors
+-- 			undercurl = true,
+-- 			underline = true,
+-- 			bold = true,
+-- 			italic = {
+-- 				strings = false,
+-- 				emphasis = false,
+-- 				comments = false,
+-- 				operators = false,
+-- 				folds = false,
+-- 			},
+-- 			strikethrough = true,
+-- 			invert_selection = false,
+-- 			invert_signs = false,
+-- 			invert_tabline = false,
+-- 			invert_intend_guides = false,
+-- 			inverse = true, -- invert background for search, diffs, statuslines and errors
+-- 			contrast = "", -- can be "hard", "soft" or empty string
+-- 			palette_overrides = {},
+-- 			overrides = {},
+-- 			dim_inactive = false,
+-- 			transparent_mode = true,
+-- 		})
+-- 		vim.o.background = "dark" -- or "light" for light mode
+-- 		vim.cmd("colorscheme gruvbox")
+-- 	end,
+-- }
 --
 -- return {
 -- 	"rose-pine",
@@ -206,36 +206,36 @@ return {
 --   end,
 -- }
 
--- function ColorMyPencils(color)
--- 	color = color or "rose-pine"
--- 	vim.cmd.colorscheme(color)
--- end
---
--- return {
--- 	{
--- 		"rose-pine/neovim",
--- 		name = "rose-pine",
--- 		config = function()
--- 			require("rose-pine").setup({
--- 				disable_background = true,
--- 				styles = {
--- 					bold = true,
--- 					italic = false,
--- 					transparency = true,
--- 				},
--- 				highlight_groups = {
--- 					Cursor = { fg = "black", bg = "#ffffff" }, -- Cursor color white
--- 				},
--- 			})
---
--- 			vim.cmd("colorscheme rose-pine")
--- 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
---
--- 			ColorMyPencils()
--- 		end,
--- 	},
--- }
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+end
+
+return {
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = true,
+				},
+				highlight_groups = {
+					Cursor = { fg = "black", bg = "#ffffff" }, -- Cursor color white
+				},
+			})
+
+			vim.cmd("colorscheme rose-pine")
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+			ColorMyPencils()
+		end,
+	},
+}
 --
 -- return {
 --   "rebelot/kanagawa.nvim", -- Repositorio del colorscheme
