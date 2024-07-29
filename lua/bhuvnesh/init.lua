ad612970c78db0d288192a6967a7a6aa774854a1
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-07-29 14:30:47 +0530

    tweeks

--- lua/bhuvnesh/init.lua
+++ lua/bhuvnesh/init.lua
@@ -1,22 +1,24 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.updatetime = 50
---
+
+require("bhuvnesh.set")
+require("bhuvnesh.remap")
+require("bhuvnesh.lazy_init")
 --NOTE:Below two line disable netrw flash.
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 --
+vim.opt.termguicolors = true
+
 -- Set the sign column to a fixed width of 2
 vim.o.signcolumn = "yes"
 
 -- Increase the number column width to include padding
 vim.opt.guicursor = "n-v-i-c:block-Cursor"
 
-require("bhuvnesh.set")
-require("bhuvnesh.remap")
-require("bhuvnesh.lazy_init")
 
-vim.o.pumheight = 10 --:NOTE:To prevent autocomplete menu large height
+-- vim.o.pumheight = 15 --:NOTE:To prevent autocomplete menu large height
 --
 vim.api.nvim_set_hl(0, 'Cursor', { fg = 'NONE', bg = 'NONE' })
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -71,19 +73,21 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-local o = vim.o
-o.cursorline = false
-o.expandtab = true
-o.smartindent = true
-o.tabstop = 2
-o.shiftwidth = 4
-
-vim.opt.list = false
-vim.scriptencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-vim.opt.colorcolumn = "75" --- colorcolumn (disable to remove colorcolumn)
-vim.opt.isfname:append("@-@")
+--Commented ----> :NOTE:
+-- local o = vim.o
+-- -- o.cursorline = false
+-- o.expandtab = true
+-- o.smartindent = true
+-- o.tabstop = 2
+-- o.shiftwidth = 4
+--
+-- vim.opt.list = false
+-- vim.scriptencoding = "utf-8"
+-- vim.opt.encoding = "utf-8"
+-- vim.opt.fileencoding = "utf-8"
+-- vim.opt.colorcolumn = "75" --- colorcolumn (disable to remove colorcolumn)
+-- vim.opt.isfname:append("@-@")
+------> Till here is commented. :NOTE:
 
 --Gives better highlight for gruvbox theme, comment out below 2 lines
 --while using other themes.
@@ -92,46 +96,52 @@ vim.opt.isfname:append("@-@")
 -- Set the background color for the selected item to a lighter black and the text color to cyan
 -- vim.cmd("highlight PmenuSel ctermbg=black ctermfg=cyan guibg=#1c1c1c guifg=#00ffff")
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.backup = false
+----COmmented ----> :NOTE:
+-- vim.opt.number = true
+-- vim.opt.relativenumber = true
+-- vim.opt.termguicolors = true
+-- -- vim.opt.title = true
+-- vim.opt.autoindent = true
+-- vim.opt.smartindent = true
+-- vim.opt.hlsearch = false
+-- vim.opt.incsearch = true
+-- vim.opt.backup = false
 --
 --UNCOMMENT IF ANYTHING IN STATUS LINE BREAKS
-vim.opt.showcmd = true -- vim.opt this to true for status line
-vim.opt.cmdheight = 1 -- vim.opt to 1 if you want to see primagen like status line, plus comment out
+-- vim.opt.showcmd = true -- vim.opt this to true for status line
+-- vim.opt.cmdheight = 1 -- vim.opt to 1 if you want to see primagen like status line, plus comment out
 -- some code in noice, lua and nvim-notify.
-vim.opt.laststatus = 2 -- vim.opt this to 1 as well for status line.
+-- vim.opt.laststatus = 2 -- vim.opt this to 1 as well for status line.
+-------------->>>Till here is commented. :NOTE:
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.scrolloff = 8
-vim.opt.inccommand = "split"
-vim.opt.ignorecase = true
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.wrap = false
 
--- Uncomment line 122 - 130
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.path:append({ "**" })
-vim.opt.wildignore:append({ "*/node_modules/*" })
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
-vim.opt.showmode = true -- need to vim.opt it true to show commands in status line like prime.
+------Commented ----->:NOTE:
+-- vim.opt.scrolloff = 8
+-- vim.opt.inccommand = "split"
+-- vim.opt.ignorecase = true
+-- vim.opt.smarttab = true
+-- vim.opt.breakindent = true
+-- vim.opt.wrap = false
 --
+-- Uncomment line 122 - 130
+-- vim.opt.backspace = { "start", "eol", "indent" }
+-- vim.opt.path:append({ "**" })
+-- vim.opt.wildignore:append({ "*/node_modules/*" })
+-- vim.opt.splitbelow = true
+-- vim.opt.splitright = true
+-- vim.opt.splitkeep = "cursor"
+-- vim.opt.mouse = ""
+-- vim.opt.showmode = true -- need to vim.opt it true to show commands in status line like prime.
+--------------Till here commented. :NOTE:
+
+
+
 -- -- Add asterisks in block comments
 -- vim.opt.formatoptions:append({ "r" })
 --Uncomment below line to get full file path
 -- vim.opt.statusline = [[%<%F %h%m%r %y %=%{v:register} %-14.(%l,%c%V%) %p%%]] -- for the status line
--- -- vim.api.nvim_set_keymap("n", "<leader>e", ":Lexplore<CR>", { noremap = true, silent = true }) --netrw binding.
 
 -- IMPORTANT **RestoreCursorPosition**
 function RestoreCursorPosition()
